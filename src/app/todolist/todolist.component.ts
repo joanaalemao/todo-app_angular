9d90723c31150f45eaf71569a55e0cda931d6c12
joanaalemao <j.alemao.developer@gmail.com>
2023-07-11 20:47:18 +0100

    Initial commit

---
+++ src/app/todolist/todolist.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-todolist',
+  templateUrl: './todolist.component.html',
+  styleUrls: ['./todolist.component.css']
+})
+export class TodolistComponent {
+
+taskArray = [{taskName: 'Brush teeth', isCompleted: false}];
+
+onSubmit(form: NgForm) {
+
+  this.taskArray.push({
+
+    taskName: form.controls['task'].value,
+    isCompleted: false
+    })
+
+  form.reset();
+
+}
+
+onDelete(index: number){
+  this.taskArray.splice(index, 1)
+
+}
+
+onCheck(index: number){
+  
+  this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted;
+
+}
+
+}
